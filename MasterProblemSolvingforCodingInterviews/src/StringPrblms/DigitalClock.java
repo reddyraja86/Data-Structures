ad57188e4bce9130e5e5919ceb5ea9ec61d2f6d4
Raju Rayappa <reddyraja86@gmail.com>
2022-06-01 20:34:53 +0530

    1 st june Checkin

---
+++ MasterProblemSolvingforCodingInterviews/src/StringPrblms/DigitalClock.java
@@ -0,0 +1,17 @@
+package StringPrblms;
+
+/**
+ * Covert input to digital clock Format
+ * 
+ * Ex:
+ * 
+ * 1180 will be 19:40
+ * 125 will be 2:05
+ * 155 will be 2:35
+ * 
+ */
+public class DigitalClock {
+    public static void main(String[] args) {
+
+    }
+}
