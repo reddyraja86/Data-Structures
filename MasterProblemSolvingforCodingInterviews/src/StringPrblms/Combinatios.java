44702a82cf2937239f4576391ddff15cab51ee2e
Raju Rayappa <reddyraja86@gmail.com>
2022-06-01 20:17:20 +0530

    1 st june Checkin

--- MasterProblemSolvingforCodingInterviews/src/StringPrblms/Combinatios.java
+++ MasterProblemSolvingforCodingInterviews/src/StringPrblms/Combinatios.java
@@ -13,7 +13,19 @@ package StringPrblms;
 public class Combinatios {
 
     public static void main(String[] args) {
+        String val = "abc";
+        Combinatios c = new Combinatios();
+        c.findAll("", 0, 3, val);
 
+    }
+
+    void findAll(String s, int index, int n, String val) {
+        if (index == n) {
+            System.out.print(s + "  ");
+            return;
+        }
+        findAll(s + val.charAt(index), index + 1, n, val);
+        findAll(s, index + 1, n, val);
     }
 
 }
