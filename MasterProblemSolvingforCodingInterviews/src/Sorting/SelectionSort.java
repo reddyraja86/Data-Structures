1b5017b738f9a3e8c7c5b19be5ca4209fa31d475
Raju Rayappa <reddyraja86@gmail.com>
2022-06-02 12:39:54 +0530

    2nd june Checkin

---
+++ MasterProblemSolvingforCodingInterviews/src/Sorting/SelectionSort.java
@@ -0,0 +1,22 @@
+package Sorting;
+
+/*
+
+In selection sort we will 
+    -- GO through the array and  find smallest element
+    -- Swap that element wiht the first element
+    -- DO the avobe two steps till end of all elements
+
+*/
+public class SelectionSort {
+    static void valuesInArray(int a[]) {
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + "  ");
+        }
+    }
+
+    public static void main(String[] args) {
+        int a[] = { 7, 8, 3, 2, 1 };
+
+    }
+}
