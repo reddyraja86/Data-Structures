81d31dede55088f971db6dd617b0343531ec5fee
Raju Rayappa <reddyraja86@gmail.com>
2022-06-08 16:19:19 +0530

    8th June Commit

--- MasterProblemSolvingforCodingInterviews/src/ApnaCollege/Arrays/Problem7.java
+++ MasterProblemSolvingforCodingInterviews/src/ApnaCollege/Arrays/Problem7.java
@@ -32,17 +32,72 @@ package ApnaCollege.Arrays;
 public class Problem7 {
     public static void main(String[] args) {
 
-        int a[] = { 1, 2, 3 };
+        int a[] = { 1, 2, 4, 3 };
+
+        // 1234 1243 1324 1342
+
         /**
          * 
-         * 1) find first element where a[i]<a[i+1] temp =a[i]
-         * 2) From right get the first element which is ledd than temp
+         * 1) find the first element where a[i]<a[i+1] temp =a[i]
+         * 2) From right get the first element which is less than temp
          * 3) swap element
          * 4) reverse a(i+1,last)
          */
 
-        char a[] = { '1', '2', '3', '4' };// { 'a', 'b', 'c', 'd' };
-        findAllcombsOfArrayength(a, "", 0);
+        // char a[] = { '1', '2', '3', '4' };// { 'a', 'b', 'c', 'd' };
+        // findAllcombsOfArrayength(a, "", 0);
+        nextPermutation(a);
+    }
+
+    static void nextPermutation(int a[]) {
+
+        int tempIndex = 0;
+        // find first element where a[i]<a[i+1] temp =a[i]
+        for (int i = 0; i < a.length - 1; i++) {
+            if (a[i] < a[i + 1]) {
+                tempIndex = i;
+            } else {
+                break;
+            }
+        }
+        int rightIndex = 0;
+        // From right get the first element which is less than temp
+        for (int i = a.length - 1; i > 0; i--) {
+            if (a[i] > a[tempIndex]) {
+                rightIndex = i;
+                break;
+            }
+        }
+
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i]);
+        }
+        System.out.println();
+
+        // swap the elements
+        int temp = a[tempIndex];
+        a[tempIndex] = a[rightIndex];
+        a[rightIndex] = temp;
+
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i]);
+        }
+        System.out.println();
+
+        // reverse the tempIndex+1 to length
+        tempIndex = tempIndex + 1;
+        int l = a.length - 1;
+        while (tempIndex <= l) {
+            int t = a[l];
+            a[l] = a[tempIndex];
+            a[tempIndex] = t;
+            tempIndex++;
+            l--;
+        }
+
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i]);
+        }
     }
 
     static void findAllcombsOfArrayength(char a[], String s, int x) {
---
+++ MasterProblemSolvingforCodingInterviews/src/KthLargest.java
@@ -0,0 +1,5 @@
+public class KthLargest {
+    public static void main(String[] args) {
+
+    }
+}
