cb705ad904c6fd78f3f7e725da5c678917c1d471
Raju Rayappa <reddyraja86@gmail.com>
2022-06-10 22:54:37 +0530

    10th June Commit

---
+++ MasterProblemSolvingforCodingInterviews/src/ApnaCollege/Arrays/Problem15.java
@@ -0,0 +1,44 @@
+package ApnaCollege.Arrays;
+
+public class Problem15 {
+    public static void main(String[] args) {
+        int[] nums = { 1 };
+        int k = 0;
+        int result = -1;
+        if (nums.length == 1) {
+            result = nums[0] == k ? 0 : -1;
+        } else {
+            result = search(nums, 0, nums.length - 1, k);
+        }
+
+        System.out.println(result);
+    }
+
+    static int search(int[] A, int l, int h, int k) {
+
+        int pivot = l + (h - l) / 2;
+        if (A[pivot] == k) {
+            return pivot;
+        }
+        if (h - l == 1) {
+            int val1 = A[l];
+            int val2 = A[h];
+            if (val1 == k)
+                return l;
+            else if (val2 == k)
+                return h;
+            else
+                return -1;
+        }
+
+        if (k > A[l] && k < A[pivot]) {
+            // search left
+            return search(A, l, pivot, k);
+        } else {
+            // search right
+            return search(A, pivot, h, k);
+        }
+
+    }
+
+}
